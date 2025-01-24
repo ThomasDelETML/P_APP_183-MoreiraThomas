ba831a85c3185bc4ae06a8c391f1ca4a68ffc7a7
Thomas Moreira <po51oro@eduvaud.ch>
2025-01-24 11:25:25 +0100

    login et acceuil crée

--- nodejswebshop/app/server.js
+++ nodejswebshop/app/server.js
@@ -1,17 +1,21 @@
 const express = require("express");
-
+const path = require("path");
 
 const app = express();
 
-const userRoute = require('./routes/User');
-const accueilRouute = require('./routes/accueil');
+// Définir le dossier public pour les fichiers statiques (CSS, images, etc.)
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/User', userRoute);
-app.use('/accueil', accueilRouute);
+// Définir EJS comme moteur de rendu
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // 🔥 S'assurer que Express trouve les fichiers EJS
 
-
+// Route pour afficher la page de connexion
+app.get("/login", (req, res) => {
+  res.render("login", { name: "Thomas" }); // 🔥 Utilisation de `res.render`
+});
 
 // Démarrage du serveur
 app.listen(8080, () => {
-    console.log('Server running on port 8080');
+  console.log("Server running on http://localhost:8080");
 });
